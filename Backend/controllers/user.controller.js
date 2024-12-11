68ec0e61c4728f5e59adf7192a6d2b7a1728929b
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-12-12 00:49:42 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -11,6 +11,12 @@ module.exports.registerUser = async (req, res, next) => {
 
     const { fullName, email, password } = req.body;
 
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
     const hashedPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
@@ -23,6 +29,34 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken();
 
     res.status(201).json({ token, user });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports.loginUser = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = user.generateAuthToken();
+
+    res.status(200).json({ token, user });
   } catch (error) {
     res.status(500).json(error);
   }
