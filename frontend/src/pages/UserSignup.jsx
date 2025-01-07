95dd833a16a09be0dd1922e99b02b6c57ce9b907
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-01-08 00:56:36 +0530

    added some changtes

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const UserSignUp = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userData, setUserData] = useState("");
@@ -9,9 +11,15 @@ const UserSignUp = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     setUserData({
+      username: {
+        firstName: firstName,
+        lastName: lastName,
+      },
       email: email,
       password: password,
     });
+    setFirstName("");
+    setLastName("");
     setEmail("");
     setPassword("");
   };
@@ -31,12 +39,20 @@ const UserSignUp = () => {
               required
               placeholder="First name"
               className="bg-[#eeeeee]  rounded px-4 py-2 w-1/2 border  text-base placeholder:text-sm "
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
             />{" "}
             <input
               type="text"
               required
               placeholder="Last name"
               className="bg-[#eeeeee]  rounded px-4 py-2 w-1/2 border text-base placeholder:text-sm "
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
             />
           </div>
 
@@ -46,12 +62,20 @@ const UserSignUp = () => {
             required
             placeholder="email@example.com"
             className="bg-[#eeeeee] mb-5 rounded px-4 py-2 w-full border  text-base placeholder:text-sm  "
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
           <h3 className="text-base font-medium mb-2">Enter Password</h3>
           <input
             type="password"
             placeholder="Password"
             className="bg-[#eeeeee] mb-5 rounded px-4 py-2 w-full border  text-base placeholder:text-sm  "
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <button className="bg-[#111] text-[#fff] font-semibold  mb-3 rounded px-4 py-2 w-full   text-lg placeholder:text-base ">
             Login
