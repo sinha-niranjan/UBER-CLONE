33c4d952b61f5c2c8f859382e6d4d081466687ed
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-01-08 08:10:58 +0530

    captain signup

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -11,7 +11,7 @@ const CaptainSignup = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     setUserData({
-      username: {
+      fullName: {
         firstName: firstName,
         lastName: lastName,
       },
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
 
 const Home = () => {
   return (
