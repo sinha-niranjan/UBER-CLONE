e15a711ae184da693fb9e4a93ce0abc14fff58a9
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-01-13 12:52:12 +0530

    added design for choose vehicle

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const LocationSearchPanel = () => {
-  return <div>LocationSearchPane</div>;
+  return (
+    <div>
+      {/* this is just a sample data */}
+      <div className="my-2 flex items-center justify-start gap-4">
+        <h2 className="flex h-10 w-12 items-center justify-center rounded-full bg-[#eee] p-2">
+          <i className="ri-map-pin-fill text-xl"></i>
+        </h2>
+        <h4 className="text-sm font-semibold">
+          24B, Near Kapoor's cafe, Sheryians coding School, Bhopal
+        </h4>
+      </div>
+      <div className="my-2 flex items-center justify-start gap-4">
+        <h2 className="flex h-10 w-12 items-center justify-center rounded-full bg-[#eee] p-2">
+          <i className="ri-map-pin-fill text-xl"></i>
+        </h2>
+        <h4 className="text-sm font-semibold">
+          24B, Near Kapoor's cafe, Sheryians coding School, Bhopal
+        </h4>
+      </div>
+      <div className="my-2 flex items-center justify-start gap-4">
+        <h2 className="flex h-10 w-12 items-center justify-center rounded-full bg-[#eee] p-2">
+          <i className="ri-map-pin-fill text-xl"></i>
+        </h2>
+        <h4 className="text-sm font-semibold">
+          24B, Near Kapoor's cafe, Sheryians coding School, Bhopal
+        </h4>
+      </div>
+      <div className="my-2 flex items-center justify-start gap-4">
+        <h2 className="flex h-10 w-12 items-center justify-center rounded-full bg-[#eee] p-2">
+          <i className="ri-map-pin-fill text-xl"></i>
+        </h2>
+        <h4 className="text-sm font-semibold">
+          24B, Near Kapoor's cafe, Sheryians coding School, Bhopal
+        </h4>
+      </div>
+      <div className="my-2 flex items-center justify-start gap-4">
+        <h2 className="flex h-10 w-12 items-center justify-center rounded-full bg-[#eee] p-2">
+          <i className="ri-map-pin-fill text-xl"></i>
+        </h2>
+        <h4 className="text-sm font-semibold">
+          24B, Near Kapoor's cafe, Sheryians coding School, Bhopal
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default LocationSearchPanel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -89,6 +89,60 @@ const Home = () => {
           <LocationSearchPanel />
         </div>
       </div>
+      <div className="fixed bottom-0 z-10 w-full bg-gray-100 px-3 py-8">
+        <h3 className="mb-5 text-2xl font-semibold">Choose a Vehicle</h3>
+        <div className="mb-2 flex w-full items-center justify-between rounded-xl border-2 border-gray-300 bg-white p-3 active:border-black">
+          <img className="h-12" src="car.webp" alt="car" />
+          <div className="ml-2 w-1/2">
+            <h4 className="text-sm font-semibold">
+              UberGo{" "}
+              <span>
+                <i className="ri-user-3-fill">4</i>
+              </span>
+            </h4>
+
+            <h2 className="text-sm font-medium"> 2 mins away</h2>
+            <p className="text-xs font-normal text-gray-600">
+              Affordable compact rides
+            </p>
+          </div>
+          <h2 className="text-lg font-semibold">₹193.30</h2>
+        </div>{" "}
+        <div className="mb-2 flex w-full items-center justify-between rounded-xl border-2 border-gray-300 bg-white p-3 active:border-black">
+          <img className="h-12" src="bike.webp" alt="car" />
+          <div className="ml-2 w-1/2">
+            <h4 className="text-sm font-semibold">
+              Moto
+              <span>
+                <i className="ri-user-3-fill">1</i>
+              </span>
+            </h4>
+
+            <h2 className="text-sm font-medium"> 3 mins away</h2>
+            <p className="text-xs font-normal text-gray-600">
+              Affordable motorcycle rides
+            </p>
+          </div>
+          <h2 className="text-lg font-semibold">₹63.30</h2>
+        </div>
+        <div className="mb-2 flex w-full items-center justify-between rounded-xl border-2 border-gray-300 bg-white p-3 active:border-black">
+          <img className="h-12" src="auto.webp" alt="car" />
+          <div className="ml-2 w-1/2">
+            <h4 className="text-sm font-semibold">
+              UberAuto
+              <span>
+                <i className="ri-user-3-fill">3</i>
+              </span>
+            </h4>
+
+            <h2 className="text-sm font-medium"> 3 mins away</h2>
+            <p className="text-xs font-normal text-gray-600">
+              Affordable Auto rides
+            </p>
+          </div>
+          <h2 className="text-lg font-semibold">₹113.30</h2>
+        </div>
+      </div>
     </div>
   );
 };
